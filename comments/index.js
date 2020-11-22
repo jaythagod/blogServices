0b2398adf4c5fc45193a53577dd4364e637cb14d
jtG <jude.gyimah87@gmail.com>
2020-11-22 06:13:57 +0100

    emmiting events to broker functionality added

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
@@ -12,7 +13,7 @@ app.get('/posts/:id/comments', (req, res)=>{
     res.send(commentsByPostid[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res)=>{
+app.post('/posts/:id/comments', async (req, res)=>{
 
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body;
@@ -21,8 +22,24 @@ app.post('/posts/:id/comments', (req, res)=>{
     comments.push({id: commentId, content});
     commentsByPostid[req.params.id] = comments;
 
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data:{
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+
+    });
+
     res.status(201).send(comments);
     
+});
+
+app.post('/events', (req,res)=>{
+    console.log('Recieved event', req.body.type);
+
+    res.send({});
 });
 
 
